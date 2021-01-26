3c89689f8f136e1f4a79f3d9ab71606074297a1e
VictorMardones <v.mardones04@ufromail.cl>
2021-01-26 12:46:07 -0300

    Ejercicios 2

---
+++ Ejercicios Teóricos 2/K.js
@@ -0,0 +1,4 @@
+Como expresión regular:
+K = b (a U b U c)* c (a U b U c)* a
+En javascript:
+expregK = /^b[abc]*c[abc]*a$/ ;
---
+++ Ejercicios Teóricos 2/L.js
@@ -0,0 +1,4 @@
+Como expresión regular:
+L = b (a U b U c)* a
+En javascript:
+expregL = /^b[abc]*a$/ ;
